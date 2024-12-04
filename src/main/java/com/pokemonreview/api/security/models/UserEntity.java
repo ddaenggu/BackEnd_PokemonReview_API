ccbe039907dfe79097493dabdc953e940a357cde
ddaenggu <bhm01@naver.com>
2024-12-04 17:38:35 +0900

    User/Role Repository add

--- src/main/java/com/pokemonreview/api/security/models/UserEntity.java
+++ src/main/java/com/pokemonreview/api/security/models/UserEntity.java
@@ -2,14 +2,17 @@ package com.pokemonreview.api.security.models;
 
 import jakarta.persistence.*;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "users")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 public class UserEntity {
 
@@ -20,7 +23,7 @@ public class UserEntity {
     @Column(unique = true, nullable = false)
     private String username;
     
-		@Column(nullable = false)
+    @Column(nullable = false)
     private String password;
 
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
